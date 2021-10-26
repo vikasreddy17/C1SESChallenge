dd1c9cff177a2fcf13ed23350e984887dcf095be
Vikas Thoti Reddy <vikas.red17@gmail.com>
2021-10-26 17:56:16 -0400

    Playing around with code

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Yo, my name is vikas.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/vikasreddy17"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My github account
         </a>
       </header>
     </div>
